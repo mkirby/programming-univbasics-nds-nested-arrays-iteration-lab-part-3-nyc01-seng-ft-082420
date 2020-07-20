ebca8598b68e3a213a3825f0ab4be7e24d963aa1
Matthew Kirby <matthew.robbins.kirby@gmail.com>
2020-07-20 20:20:04 +0000

    Done.

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -3,15 +3,12 @@ def join_nested_strings(src)
   count = 0
   while count < src.length do
     inner_count = 0
-    
     while inner_count < src[count].length do
-      
       if src[count][inner_count].class == String
         result_joined_strings == "" ? result_joined_strings += src[count][inner_count] : result_joined_strings += " #{src[count][inner_count]}"
       end
       inner_count += 1
     end
-    
     count += 1
   end
   p result_joined_strings
